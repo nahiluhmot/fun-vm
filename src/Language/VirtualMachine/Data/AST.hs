8f399e3e737547c193a6be90dea294414f0683ff
Tom Hulihan <hulihan.tom159@gmail.com>
2019-05-26 12:12:52 -0400

    Simplify (?) AST

--- src/Language/VirtualMachine/Data/AST.hs
+++ src/Language/VirtualMachine/Data/AST.hs
@@ -1,12 +1,11 @@
 module Language.VirtualMachine.Data.AST ( TopLevel(..)
                                         , FnStmt(..)
                                         , Stmt(..)
+                                        , LitExpr(..)
                                         , Expr(..)
-                                        , Literal
                                         ) where
 
 import Language.VirtualMachine.Data.Fix (Fix)
-import Language.VirtualMachine.Data.Value (Value)
 
 data TopLevel sym str expr stmt
   = TopLevelModule sym
@@ -14,22 +13,22 @@ data TopLevel sym str expr stmt
   | TopLevelDef sym expr
   | TopLevelStmt stmt
 
-data Stmt sym vec expr stmt
+data Stmt sym expr stmt
   = StmtExpr expr
   | StmtAssign sym expr
-  | StmtIf expr (vec stmt) (vec stmt)
+  | StmtIf expr [stmt] [stmt]
 
-data FnStmt vec expr stmt
+data FnStmt expr stmt
   = FnStmt stmt
-  | FnStmtRecur (vec expr)
+  | FnStmtRecur [expr]
 
-data Expr sym vec lit expr
+newtype LitExpr expr lit
+  = LitExpr { runLitExpr :: Fix (expr (lit (LitExpr expr lit))) }
+
+data Expr sym lit expr
   = ExprLit lit
   | ExprVar sym
-  | ExprFuncall expr (vec expr)
-  | ExprIndex expr (vec expr)
+  | ExprFuncall expr [expr]
+  | ExprIndex expr [expr]
   | ExprTernary expr expr expr
   | ExprDebugger
-
-type Literal sym int float string func seq intMap
-  = Fix (Value sym int float string func seq intMap)
