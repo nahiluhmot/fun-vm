ebdb4d28f030b25c1dfac8bba3da60072dc58a0c
Tom Hulihan <hulihan.tom159@gmail.com>
2019-05-27 13:13:15 -0400

    Add operators to the AST

--- src/Language/VirtualMachine/Data/AST.hs
+++ src/Language/VirtualMachine/Data/AST.hs
@@ -19,10 +19,13 @@ data Stmt sym expr stmt
 newtype LitExpr expr lit
   = LitExpr { runLitExpr :: Fix (expr (lit (LitExpr expr lit))) }
 
-data Expr sym lit expr
+data Expr sym op lit expr
   = ExprLit lit
   | ExprVar sym
   | ExprFuncall expr [expr]
   | ExprIndex expr expr
   | ExprTernary expr expr expr
+  | ExprParen expr
+  | ExprNot expr
+  | ExprBinOp expr op expr
   | ExprDebugger
